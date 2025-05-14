8dab5b1b64e1e8fa34f5804288470759c4db2e76
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2025-05-15 00:51:42 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 input.onGesture(Gesture.LogoUp, function () {
-    basic.pause(tempsInstallRegle)
-    for (let index = 0; index < nbLeds; index++) {
+    basic.pause(5000)
+    for (let index = 0; index < 115; index++) {
         strip.shift(1)
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
         strip.show()
-        basic.pause(dureePauseMonteeEau)
+        basic.pause(200)
     }
-    basic.pause(dureePassageRouge)
+    basic.pause(2000)
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    strip.show()
     basic.pause(5000)
-    for (let index = 0; index < Math.round(dureeStrobo / 100); index++) {
+    strip.show()
+    for (let index = 0; index < 25; index++) {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
         strip.show()
         basic.pause(100)
@@ -21,18 +21,7 @@ input.onGesture(Gesture.LogoUp, function () {
     strip.show()
 })
 let strip: neopixel.Strip = null
-let dureeStrobo = 0
-let dureePassageRouge = 0
-let dureePauseMonteeEau = 0
-let tempsInstallRegle = 0
-let nbLeds = 0
-nbLeds = 115
-let dureeMonteeEau = 30000
-tempsInstallRegle = 5000
-dureePauseMonteeEau = Math.round(dureeMonteeEau / nbLeds)
-dureePassageRouge = 2000
-dureeStrobo = 5000
-strip = neopixel.create(DigitalPin.P0, nbLeds, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 115, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 strip.show()
 basic.forever(function () {
