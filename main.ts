631c2baed9390aaa2196bc73f4083a21cf2ef841
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2025-05-14 23:54:26 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
-let z = 0
-let y = 0
-let x = 0
-let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
-while (true) {
-    x = input.acceleration(Dimension.X) / 2
-    y = input.acceleration(Dimension.Y) / 2
-    z = input.acceleration(Dimension.Z) / 2
-    strip.shift(1)
-    strip.setPixelColor(0, neopixel.rgb(x, y, 0 - z))
+input.onGesture(Gesture.LogoUp, function () {
+    basic.pause(5000)
+    for (let index = 0; index < 115; index++) {
+        strip.shift(1)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        basic.pause(200)
+    }
+    basic.pause(2000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(5000)
     strip.show()
-    basic.pause(100)
-}
+    for (let index = 0; index < 25; index++) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip.show()
+        basic.pause(100)
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        strip.show()
+    }
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    strip.show()
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 115, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+strip.show()
 basic.forever(function () {
 	
 })
